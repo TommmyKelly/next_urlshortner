d236e9ac4a8a2858cb8441f2080ece992e43ab8a
TommmyKelly <tommy_kelly@icloud.com>
2022-06-13 18:41:18 +0100

    fixed typo

--- pages/api/url.js
+++ pages/api/url.js
@@ -1,9 +1,10 @@
 import connectDB from "../../middleware/mongodb";
 import Url from "../../models/urls";
 
-const create_url = () => {
-  const url = Date.now().toString(36) + Math.random().toString(36).substr(2);
-  if (!Url.findOne({ short_url: url })) {
+const create_url = async () => {
+  const url = Date.now().toString(36) + Math.random().toString(36).substring(2);
+  const item = await Url.findOne({ short_url: url });
+  if (item) {
     return create_url();
   } else {
     return url;
@@ -12,16 +13,17 @@ const create_url = () => {
 
 const handler = async (req, res) => {
   if (req.method === "POST") {
-    // Check if long_url is provided
+    // Check if long_url is providedSS
     const { long_url } = req.body;
     if (long_url) {
       try {
         const check_url = await Url.findOne({ long_url });
+
         if (check_url) {
           res.status(200).json({ short_url: check_url.short_url });
         } else {
           const url = new Url({
-            short_url: create_url(),
+            short_url: await create_url(),
             long_url,
           });
           // Create new user
